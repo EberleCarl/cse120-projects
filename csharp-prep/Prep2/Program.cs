df765dbef5a49b3f028302e7f910bbb4a76936b8
Carl Eberle <ebe23012@byui.edu>
2026-09-18 17:22:11 -0600

    added and refined the grade analyzer

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("WHat is your grade percentage in this Class? ");
+        string Grade = Console.ReadLine();
+        int percent = int.Parse(Grade);
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade in the class is: {letter}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the Class!");
+        }
+        else
+        {
+            Console.WriteLine($"You Failed the class with a {letter}");
+        }
+
+
     }
 }
